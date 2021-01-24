707d9130d8d6d56dce7a6e3eb47a33bebcbb41d2
Stuart Jansen <sjansen@buscaluz.org>
2021-01-23 17:55:34 -0700

    initial commit

---
+++ terraform/env/production/main.tf
@@ -0,0 +1,15 @@
+module "app" {
+  source = "../../modules/app"
+  
+  dns_name = "figlet.rocks"
+  dns_zone = "figlet.rocks"
+  tags = {
+    Environment = local.env
+    Project     = local.proj
+  }
+
+  providers = {
+    aws = aws
+    aws.us-east-1 = aws.us-east-1
+  }
+}
